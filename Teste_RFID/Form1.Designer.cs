c895a3556949c63afccc9092bf635e43409a4e47
devfelipesouza <86885858+devfelipesouza@users.noreply.github.com>
2021-07-03 19:04:40 -0300

    Inicial

---
+++ Teste_RFID/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Teste_RFID
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBlock1 = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBlock1
+            // 
+            this.textBlock1.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBlock1.Location = new System.Drawing.Point(72, 65);
+            this.textBlock1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBlock1.Name = "textBlock1";
+            this.textBlock1.Size = new System.Drawing.Size(251, 31);
+            this.textBlock1.TabIndex = 1;
+            this.textBlock1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(142, 99);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Código RFID";
+            // 
+            // button1
+            // 
+            this.button1.Image = global::Teste_RFID.Properties.Resources.icons8_bank_cards_48;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(126, 12);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ler Cartão";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 129);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBlock1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Realiza Leitura de Cartão RFID";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox textBlock1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Teste_RFID/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teste_RFID
+{
+    public partial class Form1 : Form
+    {
+        int retCode;
+        int hCard;
+        int hContext;
+        int Protocol;
+        public bool connActive = false;
+        string readername = "ACS ACR122 0";      // change depending on reader
+        public byte[] SendBuff = new byte[263];
+        public byte[] RecvBuff = new byte[263];
+        public int SendLen, RecvLen, nBytesRet, reqType, Aprotocol, dwProtocol, cbPciLength;
+        public Card.SCARD_READERSTATE RdrState;
+        public Card.SCARD_IO_REQUEST pioSendRequest;       public Form1()
+        {
+            InitializeComponent();
+            SelectDevice();
+            establishContext();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (connectCard())
+            {
+                string cardUID = getcardUID();
+                textBlock1.Text = cardUID; //displaying on text block
+            }
+        }
+
+        public bool connectCard()
+        {
+            connActive = true;
+
+            retCode = Card.SCardConnect(hContext
+                                      , readername
+                                      , Card.SCARD_SHARE_SHARED
+                                      , Card.SCARD_PROTOCOL_T0 | Card.SCARD_PROTOCOL_T1
+                                      , ref hCard
+                                      , ref Protocol);
+
+            if (retCode != Card.SCARD_S_SUCCESS)
+            {
+                MessageBox.Show(Card.GetScardErrMsg(retCode), "Cartão não disponível", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                connActive = false;
+                return false;
+            }
+            return true;
+        }
+
+        private string getcardUID() //only for mifare 1k cards
+        {
+            string cardUID = "";
+            byte[] receivedUID = new byte[256];
+            Card.SCARD_IO_REQUEST request = new Card.SCARD_IO_REQUEST();
+            request.dwProtocol = Card.SCARD_PROTOCOL_T1;
+            request.cbPciLength = System.Runtime.InteropServices.Marshal.SizeOf(typeof(Card.SCARD_IO_REQUEST));
+            byte[] sendBytes = new byte[] { 0xFF, 0xCA, 0x00, 0x00, 0x00 }; //get UID command      for Mifare cards
+            int outBytes = receivedUID.Length;
+
+            int status = Card.SCardTransmit(hCard
+                                          , ref request
+                                          , ref sendBytes[0]
+                                          , sendBytes.Length
+                                          , ref request
+                                          , ref receivedUID[0]
+                                          , ref outBytes);
+
+            if (status != Card.SCARD_S_SUCCESS)
+            {
+                cardUID = "Error";
+            }
+            else
+            {
+                cardUID = BitConverter.ToString(receivedUID.Take(4).ToArray()).Replace("-", string.Empty).ToLower();
+            }
+
+            return cardUID;
+        }
+
+
+        public void SelectDevice()
+        {
+            List<string> availableReaders = this.ListReaders();
+            this.RdrState = new Card.SCARD_READERSTATE();
+            readername = availableReaders[0].ToString();//selecting first device
+            this.RdrState.RdrName = readername;
+        }
+
+        public List<string> ListReaders()
+        {
+            int ReaderCount = 0;
+            List<string> AvailableReaderList = new List<string>();
+
+            //Make sure a context has been established before 
+            //retrieving the list of smartcard readers.
+            retCode = Card.SCardListReaders(hContext, null, null, ref ReaderCount);
+            if (retCode != Card.SCARD_S_SUCCESS)
+            {
+                MessageBox.Show(Card.GetScardErrMsg(retCode));
+                //connActive = false;
+            }
+
+            byte[] ReadersList = new byte[ReaderCount];
+
+            //Get the list of reader present again but this time add sReaderGroup, retData as 2rd & 3rd parameter respectively.
+            retCode = Card.SCardListReaders(hContext, null, ReadersList, ref ReaderCount);
+            if (retCode != Card.SCARD_S_SUCCESS)
+            {
+                MessageBox.Show(Card.GetScardErrMsg(retCode));
+            }
+
+            string rName = "";
+            int indx = 0;
+            if (ReaderCount > 0)
+            {
+                // Convert reader buffer to string
+                while (ReadersList[indx] != 0)
+                {
+
+                    while (ReadersList[indx] != 0)
+                    {
+                        rName = rName + (char)ReadersList[indx];
+                        indx = indx + 1;
+                    }
+
+                    //Add reader name to list
+                    AvailableReaderList.Add(rName);
+                    rName = "";
+                    indx = indx + 1;
+
+                }
+            }
+            return AvailableReaderList;
+
+        }
+
+        internal void establishContext()
+        {
+            retCode = Card.SCardEstablishContext(Card.SCARD_SCOPE_SYSTEM, 0, 0, ref hContext);
+            if (retCode != Card.SCARD_S_SUCCESS)
+            {
+                MessageBox.Show("Verifique o dispositivo e tente novamente", "Leitor não conectado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                connActive = false;
+                return;
+            }
+        }
+    }
+}
